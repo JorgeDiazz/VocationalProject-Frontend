e730be5c679e2c7bbabe6eff1800deb5e488650c
Kate Morales <katemorales@iMac-de-Kate.local>
2019-08-30 22:44:29 -0500

    GUI LogIn completo

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -14,12 +14,45 @@ import Swal from 'sweetalert2';
  * Y dejo acceder al lugar correspondiente 
  */
 
-export class LoginComponent implements OnInit {
 
-  constructor() { }
+export class LoginComponent implements OnInit {
+ 
+  private log:boolean;
+  private reg:string;
+  private pos:boolean;
+  private bot1:string;
+  private bot2:string;
+
+  constructor() {
+    this.log= false;
+    this.reg="Register colorLog2";
+    this.bot1=" buttonLogIn3 pos colorLog1 buttonLogIn3Select";
+    this.bot2=" buttonLogIn3 colorLog1";
+   }
 
   ngOnInit() {
-    
+   
   }
+
+  change(){
+  if (this.log) {
+    this.log=false;
+    this.reg="Register colorLog2 movLeft";
+  } else {
+    this.log=true;
+    this.reg="Register colorLog2 movRight";
+  }
+  }
+  changePM(){
+    if (this.pos) {
+      this.pos=false;
+      this.bot1=" buttonLogIn3 pos colorLog1 buttonLogIn3Select";
+      this.bot2=" buttonLogIn3 colorLog1";
+    } else {
+      this.pos=true;
+      this.bot1=" buttonLogIn3 pos colorLog1";
+      this.bot2=" buttonLogIn3 colorLog1  buttonLogIn3Select";
+    }
+    }
 
 }
