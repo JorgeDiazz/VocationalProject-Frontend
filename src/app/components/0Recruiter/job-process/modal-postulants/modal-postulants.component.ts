abc009f7d639dba7db8120341cca0a25ad0371c8
Kate Morales <katemorales@iMac-de-Kate.local>
2019-11-26 12:21:48 -0500

    ajustes perfil de los postulantes

--- src/app/components/0Recruiter/job-process/modal-postulants/modal-postulants.component.ts
+++ src/app/components/0Recruiter/job-process/modal-postulants/modal-postulants.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { MAT_DIALOG_DATA } from '@angular/material';
 import { ServiceService } from 'src/app/services/service.service';
 import { PostulantI } from '../../../../models/models.model';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-modal-postulants',
@@ -11,14 +12,33 @@ import { PostulantI } from '../../../../models/models.model';
 export class ModalPostulantsComponent implements OnInit {
 
   postulants:PostulantI[];
+  form:FormGroup;
+  postulans1={};
   constructor(@Inject(MAT_DIALOG_DATA) public data: any, private serv:ServiceService) { 
-   this.serv.Vacant.GetPostulants(data.id).subscribe(data => {
-    this.postulants = <PostulantI[]>data.body;
-    console.log(this.postulants)
-  })
+    this.form = new FormGroup({
+      postulants: new FormControl()
+   });
   }
 
   ngOnInit() {
+  }
+
+  getPostulants(){
+    this.serv.Vacant.GetPostulants(this.data.id).subscribe(data => {
+      this.postulants = <PostulantI[]>data.body;
+      for (let i = 0; i < this.postulants.length; i++) {
+        this.postulans1[this.postulans1[i].id]="no";
+      }
+      console.log(this.postulants)
+    })
+  }
+  listaPostulantes(id:number){
+    if(this.postulans1[id]=="no"){
+      this.postulans1[id]="yes";
+    }
+    if(this.postulans1[id]=="yes"){
+      this.postulans1[id]="no";
+    }
   }
 
 }
